9500abcb1a0a480a68f5e1bf1fd3fae8a50ee2a9
rrovshanamammadova <rrovshanamammadova@gmail.com>
2026-01-09 22:31:24 +0400

    task 20 added

---
+++ task-20/hometask-20.js
@@ -0,0 +1,94 @@
+//Task 1
+// let bookPrice = 750;
+// let bookAmount = 14;
+// console.log(bookPrice * bookAmount);
+
+//Task 2
+// let kmhSpeed=75;
+// let m=75*1000;
+// let msSpeed=m/3600
+// console.log(msSpeed);
+
+//Task 3
+// let birthDate = prompt("Doğum tarixini daxil edin (YYYY-MM-DD):");
+
+//Task 4
+// let num=25;
+// console.log(num**5);
+// console.log(num%3);
+
+// Task 5
+// let firstName=  `Alan`;
+// let lastName=   `Turing`;
+// console.log(`Sizin adiniz ${firstName} ${lastName}. Tanişliğimiza çox şadam!`)
+
+//Task 7
+// let fullName=`Murad Orucov`
+// console.log(fullName.length);
+
+//Task 8
+// let fullName = prompt("Ad və soyadınızı daxil edin");
+// let basHerf =fullName[0] +fullName[fullName.indexOf(" ") + 1];
+// console.log(basHerf);
+
+//Task 9
+// let fullName = prompt("Tam adinizi 'AD SOYAD' formatinda daxil edin");
+// let parts = fullName.split(" ");
+// let deyisilmis = parts[1] + " " + parts[0];
+// console.log(deyisilmis);
+
+//Task 10
+// let firstName=prompt(` Adinizi daxil edin`);
+// console.log(`Salam,` + firstName + `!`);
+
+//Task 11
+// let eded1=Number(prompt("Birinci ədədi daxil edin:"));
+// let eded2=Number(prompt("ikinci ədədi daxil edin:"));
+// console.log("Toplama: " + (eded1 + eded2));
+// console.log("Çıxma: " + (eded11 - eded2));
+// console.log("Vurma: " + (eded1 * eded2));
+// console.log("Bölmə: " + (eded1 / eded2));
+
+//Task 12
+// let num1=12345;
+// let num2=98765;
+// let c=num1*num2;
+// let str=String(c);
+// console.log(c);
+// console.log(str.charAt(5));
+
+//Task 13
+// "10", -1, NaN, 2, "9px", "$45", 2, "NaNpx", infinity, "-95", -14, 1, NaN, 
+
+//Task 14
+// let eded=Number(prompt("4reqemli ədədi daxil edin:"));
+// let numStr = String(eded);
+// let cem = Number(numStr[0]) + Number(numStr[1]) + Number(numStr[2]) + Number(numStr[3]);
+// console.log("Rəqəmlərin cəmi: " + cem);
+
+//Task 16
+// let qiymet = prompt("Qiymeti daxil edin");
+// let parts = qiymet.split(".");
+// let text=parts[0]+"manat"+ " " +parts[1]+"qepik";
+// console.log(text);
+
+//Task 17
+// let emekhaqqi=Number(prompt("Emekhaqqinizi daxil edin:"));
+// let vergisiz=emekhaqqi* (1 - 0.13);
+// console.log(vergisiz);
+
+//Task 18
+// let fullName = prompt("Soyad, ad və ata adınızı daxil edin:");
+// let parts = fullName.split(" ");
+// let basherf = parts[1][0].toUpperCase() + "." + parts[2][0].toUpperCase() + ".";
+// let yekun = parts[0] + " " + initials;
+
+// console.log(yekun);
+
+//Task 19
+// let value = 10;
+// console.log(value == 10);  
+// console.log(value === 10); 
+// console.log(value >= 10); 
+// console.log(value <= 10);  
+// console.log(value != 20);  
