5bbfd3ad9111a3054504d2dceb0f4c5fee6dd490
rrovshanamammadova <rrovshanamammadova@gmail.com>
2026-01-09 22:50:07 +0400

    task 19 added

---
+++ task-19/task19.js
@@ -0,0 +1,15 @@
+//Task 1
+// console.log("Hello world");
+
+//task 2
+// const myName = "Rovsane";
+// console.log(myName);
+
+//task 3
+// person, $add
+
+//task 4
+// let firstName = prompt("Adınızı daxil edin:");
+// let lastName = prompt("Soyadınızı daxil edin:");
+
+// console.log("İstifadəçi: " + firstName + " " + lastName);
