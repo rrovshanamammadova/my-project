599983d8e809eab43bb0f9d654a9f186bbf4928b
rrovshanamammadova <rrovshanamammadova@gmail.com>
2026-01-15 08:40:56 +0400

    new task added

---
+++ task-21/task21.js
@@ -0,0 +1,52 @@
+//Task 20
+// let age = prompt("Yasinizi daxil edin:");
+// if(age<18){
+//     console.log("Access denied")
+// } else{
+//     console.log("Access granted")
+// }
+
+
+//Task 21
+// let num = prompt("3 eded ikireqemli daxil edin:");
+// let parts = num.split(" ");
+// let a=Number(parts[0]);
+// let b=Number(parts[1]);
+// let c=Number(parts[2]);
+// if(a===b|| a===c|| b===c){
+//     console.log("Xeta! Zehmet olmasa ferqli ededler daxil edin")
+// }
+// else{
+//     if ((a > b && a < c) || (a < b && a > c)){
+//         console.log(a);
+//     }
+//     else if((b > a && b < c) || (b < a && b > c)){
+//         console.log(b);
+//     }
+//     else{
+//         console.log(c);
+//     }
+// }
+
+
+//Task 22
+// let il= +prompt("Il daxil edin:");
+// if(il%4==0){
+//     console.log("Il uzun ildir");
+// }
+// else{
+//     console.log("Il uzun il deyil");
+// }
+
+
+//Task 23
+// let cins=prompt("Cinsi daxil edin:");
+// if(cins==="M"){
+//     console.log("Gender is Male!");
+// }
+// else if(cins==="F"){
+//     console.log("Gender is Female!");
+// }
+// else{
+//     console.log("Gender is not found!");
+// }
