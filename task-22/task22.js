7844034571d77fe76f7d649f6087b76ad7c399ee
rrovshanamammadova <rrovshanamammadova@gmail.com>
2026-01-16 21:00:46 +0400

    task 22 added

---
+++ task-22/task22.js
@@ -0,0 +1,82 @@
+//Task 1
+// let num = +prompt("Ikireqemli daxil edin:");
+// while(num<100){
+//     num+=7;
+// }
+// console.log(num);
+
+
+//Task 2
+// let num = +prompt("Eded daxil edin:");
+// for(let i=1; i<=num; i++){
+//     console.log("I know how to use cycles");
+// }
+
+
+//Task 3
+// for (let i = 100; i <= 999; i++) {
+//   if (i % 10 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+//Task 4
+// let a=0;
+// for(let i=10; i<100; i++){
+//     if(i%2!==0){
+//         a+=i
+//     }
+// }
+// console.log(a);
+
+
+//Task 5
+// let sum=0;
+// let num = +prompt("Eded daxil edin:");
+// for(let i=100; i<1000; i+=15){
+//     sum+=i;   
+// }
+// console.log(sum);
+
+
+//Task 6
+
+
+//Task 7
+// let p=1;
+// for (let i = 1; i <= 10; i++){
+//     p*=i;
+// }
+// console.log(p);
+
+
+//Task 8
+// let text = prompt("Mətni daxil edin:");
+// let tersineText = "";
+// let i = text.length - 1;
+
+// while (i >= 0) {
+//   tersineText += text[i];
+//   i--;
+// }
+// console.log(tersineText);
+
+
+//Task 9
+// let text = prompt("Mətni daxil edin:");
+// let i = 0;
+// let index = -1;
+
+// while (i < text.length) {
+//   if (text[i] === ".") {
+//     index = i;
+//     break;
+//   }
+//   i++;
+// }
+// console.log(index);
+
+
+//Task 10
+
